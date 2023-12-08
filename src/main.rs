b630a4add36f1db9bfad5b5107b6e0c8f0c99b2b
Illia Stetsenko <illia.stetsenko@netstalker.io>
2023-12-08 10:52:48 +0200

    feature/CU-86939ue0d: added initial rust project structure

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    init_log();
+    log::info!("run net-gateway service");
+}
+
+fn init_log() {
+    let config_str = include_str!("log4rs.yml");
+    let config = serde_yaml::from_str(config_str).unwrap();
+    log4rs::init_raw_config(config).unwrap();
+}
