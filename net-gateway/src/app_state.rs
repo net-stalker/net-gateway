5b2d322fdb0ceca74ae5a4c1fd1e62d0c80c59e3
Yehor Tretiakov <yehor.tretiakov@netstalker.io>
2024-01-16 01:15:32 +0200

    feature/async-requests: Add basic async quinn usage structure for all the charts

--- net-gateway/src/app_state.rs
+++ net-gateway/src/app_state.rs
@@ -1,5 +1,9 @@
 const WEB_SOCKET_URL: &str = "ws://localhost:9091";
 
+const QUINN_CLIENT_ADDRES: &str = "0.0.0.0:0";
+const QUINN_SERVER_ADDRES: &str = "127.0.0.1:4545";
+const QUINN_SERVER_APPLICATION: &str = "localhost";
+
 #[derive(Clone, Debug)]
 pub struct AppState { }
 
@@ -9,5 +13,15 @@ impl AppState {
     }
     pub fn get_ws_url(&self) -> &'static str {
         WEB_SOCKET_URL
+    }
+
+    pub fn get_quinn_client_addres(&self) -> &'static str {
+        QUINN_CLIENT_ADDRES
+    }
+    pub fn get_quinn_server_addres(&self) -> &'static str {
+        QUINN_SERVER_ADDRES
+    }
+    pub fn get_quinn_server_application(&self) -> &'static str {
+        QUINN_SERVER_APPLICATION
     }
 }
--- net-gateway/src/endpoints/charts/network_bandwidth/endpoint.rs
+++ net-gateway/src/endpoints/charts/network_bandwidth/endpoint.rs
@@ -1,16 +1,23 @@
-use actix_web::{HttpResponse, Responder, get, web, HttpRequest};
-use async_tungstenite::tungstenite::Message;
+use actix_web::get;
+use actix_web::web;
+use actix_web::Responder;
+use actix_web::HttpResponse;
+use actix_web::HttpRequest;
+
 use net_proto_api::decoder_api::Decoder;
-use net_proto_api::{typed_api::Typed, encoder_api::Encoder, envelope::envelope::Envelope};
+use net_proto_api::encoder_api::Encoder;
+use net_proto_api::envelope::envelope::Envelope;
+use net_proto_api::typed_api::Typed;
+
 use net_timescale_api::api::network_bandwidth::network_bandwidth::NetworkBandwidthDTO;
 use net_timescale_api::api::network_bandwidth::network_bandwidth_request::NetworkBandwidthRequestDTO;
-use futures::prelude::*;
-use crate::app_state::AppState;
+use net_transport::quinn::client::builder::ClientQuicConnectorBuilder;
+
 use crate::authorization::Authorization;
 use crate::authorization::mock_authenticator::MockAuthenticator;
+use crate::app_state::AppState;
 use crate::client_data::ClientData;
 use crate::endpoints::charts::network_bandwidth::chart::NetworkBandwidth;
-use async_tungstenite::tokio;
 use crate::general_filters::GeneralFilters;
 
 
@@ -21,37 +28,77 @@ async fn get_network_bandwidth(
     params: web::Query<GeneralFilters>,
     req: HttpRequest,
 ) -> impl Responder {
+    //Auth stuff
     if let Err(response) = Authorization::authorize(req, MockAuthenticator {}).await {
         return response;
     }
-    let (mut consumer, _) = tokio::connect_async(state.get_ws_url())
-        .await
-        .expect("Failed to connect");
-    let nt_request = NetworkBandwidthRequestDTO::new(params.start_date, params.end_date).encode();
-    let envelope = Envelope::new(Some(client_data.group_id.as_str()), None, NetworkBandwidthRequestDTO::get_data_type(), nt_request.as_slice());
-    consumer.send(Message::Binary(envelope.encode())).await.expect("Failed to write");
-    // TODO: move this boilerplate to a macros
-    let response = consumer.next().await;
-    let response = match response {
-        Some(Ok(response)) => response,
-        Some(Err(err)) => {
-            return HttpResponse::ExpectationFailed().json(serde_json::json!({
-                "error": err.to_string()
-            }));
-        },
-        None => {
-            return HttpResponse::ExpectationFailed().json("Failed to read");
-        }
-    };
-    let wrapped = match response {
-        Message::Binary(data) => Envelope::decode(data.as_slice()),
-        _ => {
-            return HttpResponse::ExpectationFailed().json("Failed to read")
-        }
-    };
-    if wrapped.get_type() != NetworkBandwidthDTO::get_data_type() {
-        return HttpResponse::ExpectationFailed().json("Failed to read")
+
+
+    //Form request to the server
+    let network_bandwidth_request = NetworkBandwidthRequestDTO::new(
+        params.start_date,
+        params.end_date
+    );
+    let enveloped_network_bandwidth_request = Envelope::new(
+        Some(client_data.group_id.as_str()),
+        None,
+        NetworkBandwidthRequestDTO::get_data_type(),
+        &network_bandwidth_request.encode()
+    );
+    let bytes_to_send = enveloped_network_bandwidth_request.encode();
+
+
+    //Creating Quinn Client Endpoint
+    let client_endpoint_build_result = ClientQuicConnectorBuilder::default()
+        .with_addr(state.get_quinn_client_addres().parse().unwrap())
+        .build();
+    if let Err(e) = client_endpoint_build_result {
+        //TODO: Write appropriate error returning
+        return HttpResponse::InternalServerError().body(e);
     }
-    let chart = NetworkBandwidthDTO::decode(wrapped.get_data());
-    HttpResponse::Ok().json(NetworkBandwidth::from(chart))
+    let mut client_endpoint = client_endpoint_build_result.unwrap();
+
+
+    //Connecting with Quinn Client Endpoint to the server
+    let server_connection_result = client_endpoint.connect(
+        state.get_quinn_server_addres().parse().unwrap(),
+        state.get_quinn_server_application()
+    ).await;
+    if let Err(e) = server_connection_result {
+        //TODO: Write appropriate error returning
+        return HttpResponse::InternalServerError().body(e);
+    }
+    let mut server_connection = server_connection_result.unwrap();
+
+
+    //Sending out data (request) to the server
+    let sending_result = server_connection.send_all_reliable(&bytes_to_send).await;
+    if let Err(e) = sending_result {
+        //TODO: Write appropriate error returning
+        return HttpResponse::InternalServerError().body(e);
+    }
+
+
+    //Waiting on new data and reading message from the server
+    let receiving_result = server_connection.receive_reliable().await;
+    if let Err(e) = sending_result {
+        //TODO: Write appropriate error returning
+        return HttpResponse::InternalServerError().body(e);
+    }
+    let received_bytes = receiving_result.unwrap();
+
+    let received_envelope = Envelope::decode(&received_bytes);
+
+
+    //TODO: Think about letting it all sit here. Maybe this checking is not necessary
+    if received_envelope.get_type() != NetworkBandwidthDTO::get_data_type() {
+        //TODO: Write appropriate error returning
+        return HttpResponse::InternalServerError().finish();
+    }
+
+
+    let received_chart = NetworkBandwidthDTO::decode(received_envelope.get_data());
+    let chart = NetworkBandwidth::from(received_chart);
+    
+    HttpResponse::Ok().json(chart)
 }
