0e85948bdb3a4547ec4327510e93a723683ee17c
Yehor Tretiakov <yehor.tretiakov@netstalker.io>
2024-02-09 16:39:04 +0200

    NS-72/endpoint-managers: Rename add manager logic to endpoints

--- net-gateway/src/endpoints/charts/network_graph/endpoint.rs
+++ net-gateway/src/endpoints/charts/network_graph/endpoint.rs
@@ -19,6 +19,7 @@ use crate::authorization::mock_authenticator::MockAuthenticator;
 use crate::core::app_state::AppState;
 use crate::core::client_data::ClientData;
 use crate::core::general_filters::GeneralFilters;
+use crate::core::quinn_client_endpoint_manager::QuinnClientEndpointManager;
 use crate::endpoints::charts::network_graph::chart::NetworkGraph;
 
 
@@ -50,20 +51,9 @@ async fn get_network_graph(
     let bytes_to_send = enveloped_network_graph_request.encode();
 
 
-    //Creating Quinn Client Endpoint
-    let client_endpoint_build_result = ClientQuicEndpointBuilder::default()
-        .with_addr(state.get_quinn_client_addres().parse().unwrap())
-        .build();
-    if let Err(e) = client_endpoint_build_result {
-        //TODO: Write appropriate error returning
-        return HttpResponse::InternalServerError().body(e);
-    }
-    let mut client_endpoint = client_endpoint_build_result.unwrap();
-
-
-    //Connecting with Quinn Client Endpoint to the server
-    let server_connection_result = client_endpoint.connect(
-        state.get_quinn_server_addres().parse().unwrap(),
+    let server_connection_result = QuinnClientEndpointManager::start_server_connection(
+        state.get_quinn_client_addres(),
+        state.get_quinn_server_addres(),
         state.get_quinn_server_application()
     ).await;
     if let Err(e) = server_connection_result {
