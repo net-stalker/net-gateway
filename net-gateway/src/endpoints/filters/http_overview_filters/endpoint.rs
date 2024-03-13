c6ade39be5a2e8fbe4182709f66c964da790caaa
net-illia-stetsenko <illia.stetsenko@netstalker.io>
2024-03-13 17:40:22 +0200

    NS-182/endpoint-for-http-overview-filters: implemented endpoint http overview filters

---
+++ net-gateway/src/endpoints/filters/http_overview_filters/endpoint.rs
@@ -0,0 +1,42 @@
+use std::sync::Arc;
+
+use actix_web::get;
+use actix_web::web;
+use actix_web::HttpRequest;
+use actix_web::HttpResponse;
+use actix_web::Responder;
+use net_token_verifier::fusion_auth::fusion_auth_verifier::FusionAuthVerifier;
+
+use crate::authorization;
+
+use crate::config::Config;
+use crate::core::general_filters::GeneralFilters;
+use crate::core::service_request_management::service_request_manager::ServiceRequestManager;
+use crate::endpoints::filters::http_overview_filters::request::manager::HttpOverviewFilterManager;
+
+#[get("/filter/http_overview")]
+async fn get_http_overview_filters(
+    config: web::Data<Config>,
+    params: web::Query<GeneralFilters>,
+    req: HttpRequest,
+) -> impl Responder {
+    //Auth stuff
+    let token = match authorization::authorize(req, FusionAuthVerifier::new(&config.fusion_auth_server_addres.addr, Some(config.fusion_auth_api_key.key.clone()))).await {
+        Ok(token) => token,
+        Err(response) => return response,
+    };
+
+    let filters_request_result = HttpOverviewFilterManager::default().request_data(
+        config.into_inner(),
+        Arc::new(token),
+        Arc::new(params.into_inner()),
+        None,
+    ).await;
+    if let Err(e) = filters_request_result {
+        //TODO: Write appropriate error returning
+        return HttpResponse::InternalServerError().body(e);
+    }
+    let chart = filters_request_result.unwrap();
+    
+    HttpResponse::Ok().json(chart.get_json_value())
+}
---
+++ net-gateway/src/endpoints/filters/http_overview_filters/response/http_overview_filters.rs
@@ -0,0 +1,42 @@
+use net_core_api::typed_api::Typed;
+// here we need to impl MapFilterToDTO for our DTO type which we still need to update
+use net_reporter_api::api::http_overview_dashboard_filters::http_overview_dashboard_filters::HttpOverviewDashboardFiltersDTO;
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+
+const JSON_TYPE: &str = "filters";
+
+use crate::core::service_request_management::service_response::ServiceResponse;
+
+#[derive(Serialize, Deserialize, Validate, Default, Debug, Clone)]
+pub struct HttpOverviewFiltersResponse {
+    pub endpoints: Vec<String>,
+    #[serde(rename = "httpRequestMethods")]
+    pub http_request_methods: Vec<String>,
+    #[serde(rename = "httpResponseCodes")]
+    pub http_response_codes: Vec<String>,
+}
+
+impl ServiceResponse for HttpOverviewFiltersResponse {
+    fn get_dto_type(&self) -> &'static str {
+        HttpOverviewDashboardFiltersDTO::get_data_type()
+    }
+
+    fn get_json_value(&self) -> serde_json::Value {
+        serde_json::to_value(self).unwrap()
+    }
+
+    fn get_json_type(&self) -> &'static str {
+        JSON_TYPE
+    }
+}
+
+impl From<HttpOverviewDashboardFiltersDTO> for HttpOverviewFiltersResponse {
+    fn from(value: HttpOverviewDashboardFiltersDTO) -> Self {
+        HttpOverviewFiltersResponse {
+            endpoints: value.get_endpoints().to_vec(),
+            http_request_methods: value.get_request_methods().to_vec(),
+            http_response_codes: value.get_response_codes().to_vec(),
+        }
+    }
+}
