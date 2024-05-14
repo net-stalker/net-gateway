9feb88276f839d889363c7b1b70309ba70762a48
net-illia-stetsenko <illia.stetsenko@netstalker.io>
2024-05-14 21:06:53 +0300

    finished implementing delete endpoints

--- net-gateway/src/endpoints/packets/delete_packets.rs
+++ net-gateway/src/endpoints/packets/delete_packets.rs
@@ -2,8 +2,6 @@ use actix_multipart::Multipart;
 use actix_web::delete;
 use actix_web::web;
 use actix_web::HttpRequest;
-use actix_web::HttpResponse;
-use actix_web::Responder;
 use futures::StreamExt;
 use net_core_api::api::envelope::envelope::Envelope;
 use net_core_api::api::result::result::ResultDTO;
@@ -15,6 +13,7 @@ use net_token_verifier::fusion_auth::fusion_auth_verifier::FusionAuthVerifier;
 use crate::core::quinn_client_endpoint_manager::QuinnClientEndpointManager;
 use crate::authorization;
 use crate::config::Config;
+use crate::core::user_facing_error::UserFacingError;
 
 
 #[delete("/packets")]
@@ -22,7 +21,7 @@ async fn packets(
     config: web::Data<Config>,
     req: HttpRequest,
     mut payload: Multipart
-) -> impl Responder {
+) -> Result<&'static str, UserFacingError> {
     //Auth stuff
     let token_verifier = FusionAuthVerifier::new(
         &config.fusion_auth_server_address.addr,
@@ -34,14 +33,13 @@ async fn packets(
         Box::new(token_verifier)
     ).await;
 
-    if let Err(e) = authorization_result {
-        return e;
-    }
+    if authorization_result.is_err() { return Err(UserFacingError::Unauthorized); }
+
     let token = authorization_result.unwrap();
 
     let tenant_id = token.get_tenant_id();
     if let Err(e) = tenant_id {
-        return HttpResponse::InternalServerError().body(e.to_string());
+        return Err(UserFacingError::InternalErrorWithDescription(e.to_string()));
     }
     let tenant_id = tenant_id.unwrap();
     let mut packets_ids = Vec::new();
@@ -66,35 +64,23 @@ async fn packets(
         &config.quin_inserter.addr,
         &config.quin_server_application.app,
     ).await;
+
     let mut server_connection = match server_connection_result {
         Ok(server_connection) => server_connection,
-        Err(e) => return HttpResponse::InternalServerError().body(e.to_string()),
+        Err(_) => return Err(UserFacingError::Timeout),
     };
+
     match server_connection.send_all_reliable(&request.encode()).await {
         Ok(_) => (),
-        Err(e) => return HttpResponse::InternalServerError().body(e.to_string()),
+        Err(_) => return Err(UserFacingError::Timeout),
     };
-    // the request has been sent, now I need to retrieve the response back
+
     let response = match server_connection.receive_reliable().await {
         Ok(response) => ResultDTO::decode(Envelope::decode(&response).get_data()),
-        Err(err) => return HttpResponse::InternalServerError().json(serde_json::json!({
-            "error": "Invernal Server Error",
-            "message": err.to_string()
-        })),
+        Err(_) => return Err(UserFacingError::InternalError),
     };
     match response.is_ok() {
-        true => HttpResponse::Ok().json(serde_json::json!("The packets deleted successfully")),
-        false => {
-            if response.get_description().is_err() {
-                return HttpResponse::InternalServerError().json(serde_json::json!({
-                    "error": "Invernal Server Error",
-                    "message": format!("Something went wrong during deleting the packets"),
-                }));
-            }
-            HttpResponse::InternalServerError().json(serde_json::json!({
-                "error": "Internal Server Error",
-                "message": response.get_description().unwrap(),
-            }))
-        },
+        true => Ok("The packets deleted successfully"),
+        false => Err(UserFacingError::InternalErrorWithDescription(response.get_description().unwrap_or_default().to_string())),
     }
 }
